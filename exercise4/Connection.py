bd387a0cfe86a5fd101d5e14ae89ac5cb6d36875
Olimpia Borkowska <filianda99@gmail.com>
2021-07-16 17:25:02 +0200

    Added output with avaible places

--- exercise4/Connection.py
+++ exercise4/Connection.py
@@ -16,6 +16,10 @@ yearTo = "2020"
 monthTo = "10"
 dayTo = "23"
 
+
+data_from_client = datetime.strptime('2020-10-29', date_time_format)
+data_to_client = datetime.strptime('2020-11-05', date_time_format)
+
 # collection choose
 offers = db["offers"]
 free_periods_in_all_places = []
@@ -50,18 +54,15 @@ for offer in offers.find():
     
     if len(free_periods) > 0:
         available_place = {}
+        available_place['idObject'] = offer['idObject']
         available_place['id'] = offer['id']
         available_place['free_periods'] = free_periods
         free_periods_in_all_places.append(available_place)
 
-data_from_client = datetime.strptime('2020-10-29',date_time_format)
-data_to_client = datetime.strptime('2020-11-05',date_time_format)
-
-
-for available_places in free_periods_in_all_places:
-    y = (available_places['free_periods'])
-    for avaible_period in available_places['free_periods']:
-        if data_from_client >= avaible_period['ready_from'] and data_from_client <=  avaible_period['ready_to'] and data_to_client <= avaible_period['ready_to']:
-            flag = 1 
-    if flag == 1:
-        print(available_places['id'])
+places = db["places"]
+for available_place in free_periods_in_all_places:
+    for avaible_period in available_place['free_periods']:
+        if data_from_client >= avaible_period['ready_from'] and data_to_client <= avaible_period['ready_to']:
+            place_name = places.find_one({ "id" : available_place['idObject']})['name']
+            print(place_name + " " + available_place['idObject'] + " " +  available_place['id'] + " " + avaible_period['ready_from'].strftime(date_time_format) + " " + avaible_period['ready_to'].strftime(date_time_format))
+            break
